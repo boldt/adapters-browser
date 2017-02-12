25cfc6e5a6f742fd1ff43ef8b2ba9fc0a69e6e33
Dennis Boldt <boldt@itm.uni-luebeck.de>
2017-02-12 16:27:08 +0100

    Added WebRTC

--- adapters.js
+++ adapters.js
@@ -1,9 +1,22 @@
+//
 // Adapters for browsers
+//
 
 var Adapters = {
+  // WebCrypto API
+  // https://www.w3.org/TR/WebCryptoAPI/
   crypto: window.crypto,
+
+  // Encoding API
+  // https://www.w3.org/TR/encoding/
   TextEncoder: window.TextEncoder,
-  TextDecoder: window.TextDecoder
+  TextDecoder: window.TextDecoder,
+
+  // WebRTC
+  // https://www.w3.org/TR/webrtc/
+  RTCPeerConnection: window.RTCPeerConnection,
+  SessionDescription: window.RTCSessionDescription,
+  IceCandidate: window.RTCIceCandidate
 }
 
 module.exports = Adapters;
